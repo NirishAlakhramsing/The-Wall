4924a5fd4e1bdf629d37a668b371aab5861a3fe1
Nirish Alakhramsing <alakhrn001@hva.nl>
2015-09-21 23:54:50 +0200

    Add Ore Vein and Script

---
+++ Assets/Resources/Scripts/Mine.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mine : MonoBehaviour {
+
+	private bool canMine;
+
+	// Use this for initialization
+	void Start () {
+	
+		canMine = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetMouseButtonDown(0) && canMine == true) {
+			transform.localScale -=  new Vector3(0.1f, 0.1f, 0.1f);
+		}
+
+	}
+
+	void OnMouseEnter() {
+		canMine = true;
+	}
+
+	void OnMouseExit() {
+		canMine = false;
+	}
+}
