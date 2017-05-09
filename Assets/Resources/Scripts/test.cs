b9a2bd52c2e35402beabd7d9e758a2987cee65d6
Mandy Trooster <mandytrooster@gmail.com>
2017-05-09 14:35:12 +0200

    Test Grid

---
+++ Assets/Resources/Scripts/test.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+
+    int size = 10;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        GameObject[] Cubes = new GameObject[size];
+
+
+
+        for (int i = 0; i < size; i++)
+        {
+            Cubes[i] = GameObject.Instantiate(Resources.Load("Cube") as GameObject);
+            Cubes[i].transform.position = new Vector3(i * 1.2f, 0 , 0);
+
+            for (int j = 0; j < size; j++)
+            {
+                Cubes[i] = GameObject.Instantiate(Resources.Load("Cube") as GameObject);
+                Cubes[i].transform.position = new Vector3(i * 1.2f, j * 1.2f, 0);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+}
