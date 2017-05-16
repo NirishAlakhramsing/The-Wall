f93925ac75658a4df89c8eb4311bc70df3ac35bc
Justin Hazeleger <justin.hazeleger@hva.nl>
2017-05-16 14:45:15 +0200

    working alternative generations, also locked the scaling of the UI

--- Assets/Resources/TheVeryBestFolderInThisProjectCanBeFoundRightOverHere!!!/Scripts/Grid.cs
+++ Assets/Resources/TheVeryBestFolderInThisProjectCanBeFoundRightOverHere!!!/Scripts/Grid.cs
@@ -3,13 +3,12 @@ using System.Collections;
 
 public class Grid : MonoBehaviour
 {
-
-    public float tileSize = 10;
-
+    public int tileSize = 10;
     public const int tileNumber = 16;
+
     Renderer rend;
 
-    int[] generatedGrid = new int[tileNumber * tileNumber];
+    int[,] generatedGrid = new int[tileNumber, tileNumber];
     GameObject[,] boardArray = new GameObject[tileNumber, tileNumber];
 
     //public GameObject tile;
@@ -17,42 +16,48 @@ public class Grid : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        tempRandom();
-        GenerateGrid();
+        newGeneration();
     }
 
-    public void NewGeneration()
+    public void newGeneration()
     {
         tempRandom();
-        GenerateGrid();
+        foreach (Transform child in transform)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+        boardArray = generateGrid(tileSize, generatedGrid, transform);
     }
 
     void tempRandom()
     {
         for (int i = 0; i < generatedGrid.GetLength(0); i++)
         {
-            generatedGrid[i] = (int)Random.Range(0, 3);
+            for (int j = 0; j < generatedGrid.GetLength(0); j++)
+            {
+                generatedGrid[i, j] = (int)Random.Range(0, 3);
+            }
         }
     }
 
-    void GenerateGrid()
+    public GameObject[,] generateGrid(int size, int[,] genGrid, Transform parent)
     {
-        for (int i = 0; i < boardArray.GetLength(0); i++)
+        GameObject[,] grid = new GameObject[tileNumber, tileNumber];
+
+        for (int i = 0; i < grid.GetLength(0); i++)
         {
-            for (int j = 0; j < boardArray.GetLength(1); j++)
+            for (int j = 0; j < grid.GetLength(1); j++)
             {
-                if (boardArray[i, j] == null)
-                {
-                    boardArray[i, j] = GameObject.CreatePrimitive(PrimitiveType.Plane);
-                    boardArray[i, j].transform.position = new Vector3(i * tileSize, this.transform.position.y, j * tileSize);
-                    //boardArray[i, j] = Instantiate(tile, new Vector3(j * tileSize, 0, i * tileSize), Quaternion.identity) as GameObject;
-                    boardArray[i, j].name = "cell" + i + j;
-                    boardArray[i, j].transform.parent = this.transform;
-                }
+                grid[i, j] = GameObject.CreatePrimitive(PrimitiveType.Plane);
+                grid[i, j].transform.position = parent.position + new Vector3(i * size, this.transform.position.y, j * size);
+                //boardArray[i, j] = Instantiate(tile, new Vector3(j * tileSize, 0, i * tileSize), Quaternion.identity) as GameObject;
+                grid[i, j].name = "cell" + i + j;
+                grid[i, j].transform.parent = parent;
+                grid[i, j].transform.localScale = new Vector3(0.1f * size, 1, 0.1f * size);
 
-                rend = boardArray[i, j].GetComponent<Renderer>();
+                rend = grid[i, j].GetComponent<Renderer>();
 
-                switch (generatedGrid[j + i * tileNumber])
+                switch (genGrid[i, j])
                 {
                     case 0:
                         rend.material.SetColor("_Color", Color.black);
@@ -69,5 +74,7 @@ public class Grid : MonoBehaviour
                 }
             }
         }
+
+        return grid;
     }
 }
