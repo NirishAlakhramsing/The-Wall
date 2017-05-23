5431b95481cd9e5840c208559847c7cc925299ed
Nirish Alakhramsing <nirish.alakhramsing@hva.nl>
2017-05-23 13:02:49 +0200

    temp save

---
+++ Assets/Resources/Scripts/TEST_GrowthGeneration.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    //double array voor 16 bij 16 opslag
+
+    public int tileNumber;
+    GameObject[,] boardArray;
+
+    Grid getGridScript;
+
+
+    // Use this for initialization
+    void Start () {
+        boardArray = new GameObject[tileNumber, tileNumber];
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //Look in array for the first unvisited cell
+    public void visitedCells()
+    {
+
+    }
+
+}
