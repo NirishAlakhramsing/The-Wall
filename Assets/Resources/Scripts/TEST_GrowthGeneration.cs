bb44c5b1fd8d63d9af97495f3154ba29b8987b9b
Nirish Alakhramsing <nirish.alakhramsing@hva.nl>
2017-06-06 14:14:00 +0200

    temp save

--- Assets/Resources/Scripts/TEST_GrowthGeneration.cs
+++ Assets/Resources/Scripts/TEST_GrowthGeneration.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class TEST_GrowthGeneration : MonoBehaviour {
 
     //double array voor 16 bij 16 opslag
 
@@ -16,15 +16,17 @@ public class NewBehaviourScript : MonoBehaviour {
     //GameObject[,] boardArray;
 
     private bool[] visited;
-    private int[] unvisited;
-    private int[] neighbour;
+    private int[,] unvisited;
+    private int[,] neighbour;
 
-    Grid getGridScript;
+    public Grid getGridScript;
 
     // Use this for initialization
     void Start () {
+        unvisited = new int[tileNumber, tileNumber];
+        neighbour = new int[tileNumber, tileNumber];
         //boardArray = new GameObject[tileNumber, tileNumber];
-        
+        getGridScript = GameObject.Find("Grid").GetComponent<Grid>();
     }
 	
 	// Update is called once per frame
@@ -43,13 +45,13 @@ public class NewBehaviourScript : MonoBehaviour {
             {//COLLUM
 
                 //Set all cells to unvisited
-                unvisited[i+j] = i + j;
-                Debug.Log("Cell " + i+j + " has been placed in univisited array at place " + unvisited[i]);
-
-                //Set all already occupied cells
-
+                unvisited[i,j] = 0;
+                Debug.Log("Cell " + i+ "," + j + " has been placed in univisited array at place " + unvisited[i,j]);
+                
             }
         }
+
+        GrowSeedCell();
 
         //Gather the already placed seeds
 
@@ -58,6 +60,7 @@ public class NewBehaviourScript : MonoBehaviour {
     //2 - Look in array for a seeds to grow
     public void GrowSeedCell()
     {
+        
         for (int i = 0; i < getGridScript.generatedGrid.GetLength(0); i++)
         {//ROW
             for (int j = 0; j < getGridScript.generatedGrid.GetLength(1); j++)
@@ -65,38 +68,43 @@ public class NewBehaviourScript : MonoBehaviour {
 
                 switch (getGridScript.generatedGrid[i, j])
                 {
-                    case 0: //do nothing atm
+                    case 0: //Water - do nothing atm
                         break;
+
                     case 1: //Grass - check if visited
-                           
-                            //if not - start neigbour generations
-                        if ( !visited[i + j])
+                        Debug.Log("Found a grass cell");
+
+
+                            //if not visited - start neigbour generations
+                        if (unvisited[i,j] == 0)
                         {
-                            Gather(i+j);
-                            PopulateNeigbbours();
+                            Debug.Log("cell" + i+j+ " has not been visited , commence growth");
+
+                            //GatherNeighbours(i, j);
+                            //PopulateNeighbours();
+                            //SetVisitation(i , j);
                         } else
                         {
-                            Debug.Log("Cell" + visited[i+j] + "has already been visited and calculated");
+                            Debug.Log("Cell" +i+j + " has already been visited and calculated , continue to next cell");
                         }
-
-                        
                         break;
-                    case 2:
-                        //do nothing atm
+
+                    case 2://Tree - do nothing atm
+
                         break;
                     default:
                         //cell has not been assigned by a object type
                         break;
                 }
-
-
             }
         }
     }
 
     //3 - Generate the new neigbours with new seed cells
-    public void PopulateNeigbbours()
+    public void PopulateNeighbours()
     {
+
+
         var iteration = nrOfIterations;
         var chanceGrass = growthChanceGrass;
         var chacneTree = growthChanceTree;
@@ -104,43 +112,58 @@ public class NewBehaviourScript : MonoBehaviour {
         // Chance randomizer for seed to grow in neighbour cell
         for (int i = 0; i < neighbour.Length; i++) {
         
-            //Tree
-            if ((int)Random.Range(0, 100) > growthChanceTree)
-            {
-                //change neighbour cell to tree cell (new seed)
-
-            }
-
-            //Grass
-            if ((int)Random.Range(0, 100) > growthChanceGrass)
-            {
-                //change neighbour cell to tree cell (new seed)
-
-            }
 
         }
 
     }
 
     //4 -   Gather the current populatuble neighbours of the seed cell
-    public void Gather(int seed)
+    public void GatherNeighbours(int dimOne, int dimTwo)
     {
+        // Info: 0 = univisted neighbour
+        // Info: 1 = visited neighbour
+
         //right
-        neighbour[0] = seed + 1;
+        //neighbour[dimOne, dimTwo + 1] = 0;      // seed , + 1
+        
+        getGridScript.generatedGrid[dimOne, dimTwo + 1] = 1;
         //left
-        neighbour[1] = seed - 1;
+        neighbour[dimOne, dimTwo - 1] = 0;      // seed , - 1
         //down
-        neighbour[2] = seed + 10;
+        neighbour[dimOne + 1, dimTwo] = 0;      // +1 , seed 
         //up
-        neighbour[3] = seed - 10;
+        neighbour[dimOne - 1, dimTwo] = 0;      // -1 , seed
         //corner right down
-        neighbour[4] = (seed + 10) + 1;
+        neighbour[dimOne + 1, dimTwo + 1] = 0;  // +1 , +1
         //corner right up
-        neighbour[5] = (seed - 10) + 1;
+        neighbour[dimOne - 1, dimTwo + 1] = 0;  // -1 , +1
         //corner left down
-        neighbour[7] = (seed + 10) - 1;
+        neighbour[dimOne + 1, dimTwo - 1] = 0;  // +1 , -1
         //corner left up
-        neighbour[8] = (seed - 10) - 1;
+        neighbour[dimOne - 1, dimTwo - 1] = 0;  // -1 , -1
+    }
+    
+
+    private void InitiateChance(int dimOne, int dimTwo, int type)
+    {
+
+        //Grass
+        if ((int)Random.Range(0, 100) > growthChanceGrass && type == 1)
+        {
+            //change neighbour cell to tree cell (new seed)
+            getGridScript.generatedGrid[dimOne, dimTwo] = 1;
+        }
+
+        //Tree
+        if ((int)Random.Range(0, 100) > growthChanceTree && type == 2)
+        {
+            getGridScript.generatedGrid[dimOne, dimTwo] = 2;
+        }
+
     }
 
+    private void SetVisitation(int dimOne, int dimTwo)
+    {
+        unvisited[dimOne, dimTwo] = 1; //cell has been visited
+    }
 }
