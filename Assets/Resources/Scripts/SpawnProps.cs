4d15b5af5150dcd74186a0c25a14fe6238d7aa30
Nirish Alakhramsing <alakhrn001@hva.nl>
2017-06-20 01:23:46 +0200

    Added some randomness to prevent lined perfectly lined up trees and grass

--- Assets/Resources/Scripts/SpawnProps.cs
+++ Assets/Resources/Scripts/SpawnProps.cs
@@ -21,7 +21,7 @@ public class SpawnProps : MonoBehaviour {
                 {
                     case 0:
                         gridObjects[i, j] = Instantiate(water);
-                        gridObjects[i, j].transform.position = parent.position + new Vector3(i * size, this.transform.position.y, j * size);
+                        gridObjects[i, j].transform.position = parent.position + new Vector3((i * size), this.transform.position.y, j * size);
                         gridObjects[i, j].name = "cell" + i.ToString("00") + j.ToString("00");
                         gridObjects[i, j].tag = "cell"; //tag for the cell
                         gridObjects[i, j].transform.parent = parent;
@@ -29,19 +29,20 @@ public class SpawnProps : MonoBehaviour {
                         break;
                     case 1:
                         gridObjects[i, j] = Instantiate(grass);
-                        gridObjects[i, j].transform.position = parent.position + new Vector3(i * size, this.transform.position.y, j * size);
+                        gridObjects[i, j].transform.position = parent.position + new Vector3(i * size + Random.Range(-0.50f, 0.50f), this.transform.position.y, j * size + Random.Range(-0.50f, 0.50f));
                         gridObjects[i, j].name = "cell" + i.ToString("00") + j.ToString("00");
                         gridObjects[i, j].tag = "cell"; //tag for the cell
                         gridObjects[i, j].transform.parent = parent;
-                        //gridObjects[i, j].transform.localScale = new Vector3(0.1f * size, 1, 0.1f * size);
+                        gridObjects[i, j].transform.localScale = new Vector3(Random.Range(30f, 50f), Random.Range(30f, 50f), Random.Range(20f, 40f));
+                        gridObjects[i, j].transform.localEulerAngles = new Vector3(-90f, -132.9858f, Random.Range(0f, 360f));
                         break;
                     case 2:
                         gridObjects[i, j] = Instantiate(tree);
-                        gridObjects[i, j].transform.position = parent.position + new Vector3(i * size, this.transform.position.y, j * size);
+                        gridObjects[i, j].transform.position = parent.position + new Vector3(i * size + Random.Range(-2.50f, 2.50f), this.transform.position.y, j * size + Random.Range(-2.50f, 2.50f));
                         gridObjects[i, j].name = "cell" + i.ToString("00") + j.ToString("00");
                         gridObjects[i, j].tag = "cell"; //tag for the cell
                         gridObjects[i, j].transform.parent = parent;
-                        //gridObjects[i, j].transform.localScale = new Vector3(0.1f * size, 1, 0.1f * size);
+                        gridObjects[i, j].transform.localScale = new Vector3(Random.Range(0.35f, 0.55f), 0.43f, Random.Range(0.35f, 0.7f));
                         break;
                     default:
                         break;
